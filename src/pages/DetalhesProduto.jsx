eb0f88bbec9da03c2e3c8175a0e092a3202044ce
Bianca Maria <phalves03@live.com>
2026-06-21 10:53:24 -0300

    A17

--- src/pages/DetalhesProduto.jsx
+++ src/pages/DetalhesProduto.jsx
@@ -54,16 +54,27 @@ function DetalhesProduto() {
               <strong>R$ {total.toFixed(2)}</strong>
 
               <div className="detalhes-quantidade">
-                <button onClick={diminuir}>-</button>
-                <span>{quantidade}</span>
-                <button onClick={aumentar}>+</button>
+                <button className="minus" onClick={diminuir}>-</button>
+                <span className="quantity-number">{quantidade}</span>
+                <button className="plus" onClick={aumentar}>+</button>
               </div>
             </div>
 
             <div className="detalhes-botoes">
               <button className="comprar-btn" onClick={comprar}>
-                Comprar
-              </button>
+  Comprar
+
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="18"
+    height="18"
+    fill="currentColor"
+    viewBox="0 0 16 16"
+  >
+    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .49.402L2.89 3H14.5a.5.5 0 0 1 .49.598l-1.5 7A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.49-.402L1.61 2H.5A.5.5 0 0 1 0 1.5z" />
+    <path d="M5.5 13a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm5 0a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z" />
+  </svg>
+</button>
 
               <Link to="/produtos" className="voltar-btn">
                 Voltar
